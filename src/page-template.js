14c20bd05d169535ece678c0497df98b3e98b3ac
Tommy McG <thomasjmcgovern98@gmail.com>
2022-04-24 18:25:18 -0400

    added classes and functions

--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,9 @@
 module.exports = templateData => {
     console.log(templateData);
+    // Store the value of all team members
+    const { team } = templateData;
+    // Remove the last element stored. In all cases, it will be an empty object that
+    // will only store the value 'Finish building team'
+    team.pop();
+    
 }
